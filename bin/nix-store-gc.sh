bb69ddbd1331f329a9f1031fd6665704c1ef2b70
Takamatsu Makoto <tkmtmkt@gmail.com>
2026-03-26 14:47:33 +0900

    add: シェル追加(nix-store-gc.sh)

---
+++ bin/nix-store-gc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+BASE_DIR=$(cd ${SCRIPT_DIR}/..;pwd)
+TAG=latest
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+#set -o xtrace
+
+nix store gc
+#nix-collect-garbage --delete-old
