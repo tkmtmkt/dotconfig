ef338473d843a7a89e6c0c2a4c3d143dd153ff2c
Takamatsu Makoto <tkmtmkt@gmail.com>
2026-04-09 18:20:46 +0900

    mod: fzf設定修正(仮) まだ設定が反映されない

---
+++ home-manager/extra/fzf.nix
@@ -0,0 +1,16 @@
+{
+  enable = true;
+  enableBashIntegration = true;
+
+  # fzfのデフォルトオプション
+  defaultOptions = [
+    "--height 20%"
+    "--layout=reverse"
+    "--border"
+    "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+  ];
+
+  # コマンドのエイリアス設定
+  changeDirWidgetCommand = "fd --type d";
+  fileWidgetCommand = "fd --type f";
+}
